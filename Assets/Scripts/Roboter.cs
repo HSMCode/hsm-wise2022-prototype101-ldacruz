4346e0cf0108a4d371fcc499fb191308669553ec
Linda Kruse (HSM) <linda.kruse@hs-mainz.de>
2022-11-04 10:38:47 +0100

    refactor roboter step variable

--- Assets/Scripts/Roboter.cs
+++ Assets/Scripts/Roboter.cs
@@ -4,10 +4,7 @@ using UnityEngine;
 
 public class Roboter : MonoBehaviour
 {
-    public float stepBack = 1f;
-    public float stepForward = -1f;
-    public float stepRight = -1f;
-    public float stepLeft = 1f;
+    public float step = 1f;
 
 
     // Start is called before the first frame update
@@ -19,37 +16,33 @@ public class Roboter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //transform.Translate(stepRight,0,stepBack);
-
-
         // movement on the Z axis
 
         if(Input.GetKeyDown("w"))
         {
             // move roboter back
-            transform.Translate(0,0,stepBack);
+            transform.Translate(0,0,step);
         }
 
         if(Input.GetKeyDown("s"))
         {
-            // move roboter back
-            transform.Translate(0,0,stepForward);
+            // move roboter forward
+            transform.Translate(0,0,-step);
         }
-
 
 
         // movement on the X axis
 
         if(Input.GetKeyDown("d"))
         {
-            // move roboter back
-            transform.Translate(stepLeft,0,0);
+            // move roboter right
+            transform.Translate(step,0,0);
         }
 
         if(Input.GetKeyDown("a"))
         {
-            // move roboter back
-            transform.Translate(stepRight,0,0);
+            // move roboter left
+            transform.Translate(-step,0,0);
         }
 
 
